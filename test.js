13cef9e9324c317edf77fefd4931cc6149e110fc
ydzhony1 <ydzhony1@163.com>
2019-10-26 20:37:26 +0800

    update ignore

--- test.js
+++ test.js
@@ -1,40 +1,24 @@
 /**
  * Created by jackyang on 19/10/26.
  */
-
 const fs = require('fs-extra')
-const _ = require('lodash')
-const prefix = require('global-prefix')
-const exec = require('child_process').exec
-
-var path = require('path')
-console.log('. = %s', path.resolve('.'))
-console.log('__dirname = %s', __dirname)
 
 const name = 'yang'
-const pageFile = './src1/' // + name
+const pageFile = './src/page/' + name + '/' + name + '.vue'
 const styleFile = './src/page/' + name + '/' + name + '.less'
-console.log(prefix)
-exec('xcopy ' + path.resolve(__dirname) + ' aa', (err, stdout, stderr) => {
-  // console.log('运行错误',err);
-  console.log('标准输出流', stdout)
-  // console.log('错误输出',stderr);
-})
-
 fs.pathExists(pageFile, (err, exists) => {
   if (exists) {
     console.log('this file has created')
   } else {
-    const pageFile = './src/' + name
-    fs.pathExists(pageFile, (err, exists) => {
-      if (exists) {
-        console.log('this file has created')
-      } else {
-        //var file = "wm.txt";
-        fs.ensureDir(pageFile, function(err) {
-          console.log(err)
-        })
-      }
+    fs.copy('./src/template/page.vue', pageFile, err => {
+      if (err) return console.error(err)
+
+      console.log(pageFile + '  has created')
+    })
+    fs.copy('./src/template/page.less', styleFile, err => {
+      if (err) return console.error(err)
+
+      console.log(styleFile + '  has created')
     })
   }
 })
